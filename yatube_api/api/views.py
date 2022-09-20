e31b91980950553b901324659ed764ef73045e49
Илья Ураев <uraevilya@yandex.ru>
2022-09-20 13:16:13 +0300

    add files

---
+++ yatube_api/api/views.py
@@ -0,0 +1,43 @@
+from api.serializers import CommentSerializer, GroupSerializer, PostSerializer
+from django.shortcuts import get_object_or_404
+from posts.models import Comment, Group, Post
+from rest_framework import permissions, viewsets
+from rest_framework.exceptions import PermissionDenied
+
+from .permissions import IsAuthorOrReadOnly
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied
+        super().perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied
+        instance.delete()
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return Comment.objects.filter(post=post)
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
