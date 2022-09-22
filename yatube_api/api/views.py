98661cf3477ee60cfb869f676166cbc72c844a92
Илья Ураев <uraevilya@yandex.ru>
2022-09-22 11:10:38 +0300

    delete methods update and destroy

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,13 +2,9 @@ from api.serializers import CommentSerializer, GroupSerializer, PostSerializer
 from django.shortcuts import get_object_or_404
 from posts.models import Group, Post
 from rest_framework import permissions, viewsets
-from rest_framework.exceptions import PermissionDenied
 
 from .permissions import IsAuthorOrReadOnly
 
-# 403 Forbidden Неверны авторизационные данные, указанные в запросе
-# или запрещен доступ к запрашиваемому ресурсу.
-API_RAISE_403 = PermissionDenied('Изменение чужого контента запрещено!')
 PERMISSION_CLASSES = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
 
 
@@ -24,16 +20,6 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise API_RAISE_403
-        super(PostViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise API_RAISE_403
-        instance.delete()
 
 
 class CommentViewSet(viewsets.ModelViewSet):
